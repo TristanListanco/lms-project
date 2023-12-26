b92e35aec371b91fe41d3abd88cd7ee64a7697aa
Tristan Listanco <tannyencina@gmail.com>
2023-12-26 22:34:45 +0800

    Basic Auth and Dashboard

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,16 @@
+import { SideBarRoutes } from "./sidebar-routes"
+import { Logo } from "./logo"
+
+export const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+      <div className="flex flex-col w-full">
+        <SideBarRoutes/>
+
+      </div>
+    </div>
+  )
+}
